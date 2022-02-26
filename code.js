7c17891ef9be4d48af71f7e9505b3bbf3f3e6f12
Ckandyckainz <74159422+Ckandyckainz@users.noreply.github.com>
2022-02-26 14:48:36 -0800

    v1
    v1

--- code.js
+++ code.js
@@ -4,6 +4,69 @@ mcan.width = window.innerWidth;
 mcan.height = window.innerHeight;
 let mcw = mcan.width;
 let mch = mcan.height;
+let ledgeCount = 12;
+let bc = [51/255, 38/255, 26/255];
 
-mctx.fillStyle = "#33271a";
-mctx.fillRect(0, 0, mcw, mch);
+class Ledge{
+    constructor(){
+        this.id = 0;
+        this.y = randomBetween(0.1, 0.9, 0.01);
+        this.x = randomBetween(0.1, 0.9, 0.01);
+        this.w = randomBetween(1/16, 1/4, 0.01);
+    }
+    drawSelf(ctx, cw, ch){
+        let a = (this.id/(ledgeCount-1))/2+0.25;
+        ctx.fillStyle = colorMix(...bc, 1-a, 0, 0, 0, a);
+        ctx.fillRect((this.x-this.w/2)*cw, this.y*ch, this.w*cw, ch);
+    }
+}
+
+function randomBetween(min, max, precision){
+    return Math.floor((Math.random()*(max-min)+min)/precision)*precision;
+}
+
+function colorString(r, g, b, a){
+    r = Math.floor(r*255)*256*256*256;
+    g = Math.floor(g*255)*256*256;
+    b = Math.floor(b*255)*256;
+    a = Math.floor(a*255);
+    return "#"+(r+g+b+a).toString(16).padStart(8, "0");
+}
+
+function colorMix(r1, g1, b1, a1, r2, g2, b2, a2){
+    let r = r1*a1+r2*a2;
+    let g = g1*a1+g2*a2;
+    let b = b1*a1+b2*a2;
+    let a = a1+a2;
+    return colorString(r, g, b, a);
+}
+
+let ledges = [];
+for (let i=0; i<ledgeCount; i++) {
+    let ledge = new Ledge();
+    ledges.push(ledge);
+}
+let ledgeOrder = [];
+while (ledges.length > 0) {
+    let highest = {item: 0, y: 1};
+    for (let i=0; i<ledges.length; i++) {
+        if (ledges[i].y < highest.y) {
+            highest.item = i;
+            highest.y = ledges[i].y;
+        }
+    }
+    ledges[highest.item].id = ledgeOrder.length;
+    ledgeOrder.push(ledges[highest.item]);
+    ledges.splice(highest.item, 1);
+}
+
+function drawingLoop(){
+    mctx.fillStyle = colorString(...bc, 1);
+    mctx.fillRect(0, 0, mcw, mch);
+    ledgeOrder.forEach((ledge)=>{
+        ledge.drawSelf(mctx, mcw, mch);
+    });
+    requestAnimationFrame(drawingLoop);
+}
+
+drawingLoop()
